5c976f60d5a6a3872604180a6f44f7d447f23838
d-bhanu <142707687+d-bhanu@users.noreply.github.com>
2024-03-21 14:08:12 +0530

    Add files via upload

--- MergeSort.java
+++ MergeSort.java
@@ -1,44 +1,92 @@
-import java.util.*;
-class MS{
-    public void Mergepass(int a[],int lb,int ub){
-        if(lb!=ub){
-         int mid=(lb+ub)/2;
-         Mergepass(a,lb,mid);
-         Mergepass(a,mid+1,ub);
-         Mergepass(a,lb,mid,ub);
-       }
+
+class MergeSort {
+    
+    void merge(int arr[], int l, int m, int r)
+    {
+        
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        
+        int i = 0, j = 0;
+
+        
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
     }
-    public void Mergesort(int a[],int lb,int mid,int ub){
-      int i=lb;
-      int j=mid+1;
-      int k=lb;
-      int[] temp=new int[10];
-      while(i<mid && j<=ub){
-        if(a[i]<a[j])
-          temp[k++]=a[i++];
-         else
-          temp[k++]=a[j++];
-      }
-      while(j<=ub)
-         temp[k++]=a[j++];
-      for(i=lb;i<ub;i++)
-       a[i]=temp[i];
-     }
-  }
-  public class Mergesort{
-    public static void main(String args[]){
-      Scanner Sc=new Scanner (System.in);
-      System.out.println("Enter array size");
-      int n=Sc.nextInt();
-      System.out.println("Enter the array elements");
-     int[] a=new int[n];
-   for(int i=0;i<n;i++)
-    a[i]=Sc.nextInt();
-    int lb=0,ub=n-1;
-    MS ob=new MS();
-    ob.Mergepass(a,lb,ub);
-    System.out.println("array elements after sorting");
-    for(int i=0;i<n;i++)
-       System.out.println(a[i]);
+
+    
+    void sort(int arr[], int l, int r)
+    {
+        if (l < r) {
+           
+            int m = (l + r) / 2;
+
+            
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+
+            
+            merge(arr, l, m, r);
+        }
+    }
+
+    
+    static void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+
+        System.out.println("Given Array");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        printArray(arr);
     }
 }
+
